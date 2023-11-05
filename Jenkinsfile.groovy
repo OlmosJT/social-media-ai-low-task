78ff77d2cd7156d6acf2e59918a96812a0deb42b
olmosJT <olmosjt20@gmail.com>
2023-11-06 02:56:00 +0500

    SonarQube for Gradle fixed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,7 +34,7 @@ pipeline {
             }
             steps {
                 withSonarQubeEnv("sonar-server") {
-                    bat 'cmd /c ${scannerHome}/bin/sonar-scanner.bat -X'
+                    bat 'gradle clean sonarqube'
                 }
             }
         }
